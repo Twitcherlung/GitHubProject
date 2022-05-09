bf05a26741644b1b1d045affaf4c8dc6a0f55ff2
Twitcherlung <wespe96@mail.ru>
2022-05-09 22:35:08 +0300

    Проект начат заного из-за конфликта Android Emulator Hypervisor Driver for AMD . Проект стартует с 5 урока,с устранения ошибок

---
+++ app/src/main/java/com/example/githubproject/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.githubproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.githubproject.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
